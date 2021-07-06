caabc51e7c5588b834d18b7bdb1c91494acf7d0c
xknoll56 <xavierknoll56@gmail.com>
2021-07-06 17:54:43 -0400

    Initial commit

---
+++ ScientificCalculator/Interpreter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+using DynamicExpresso;
+
+namespace ScientificCalculator
+{
+    class Interpreter : DynamicExpresso.Interpreter
+    {
+
+        private delegate double Function1d(double x);
+        private delegate double Function2d(double x, double y);
+        private delegate double FunctionNd(params double[] x);
+        private delegate double[] GetArray(params double[] x);
+
+        private double answer;
+        private double[] arr;
+
+        public Interpreter() : base(InterpreterOptions.PrimitiveTypes)
+        {
+            answer = 0.0;
+            arr = null;
+
+            //Default type must be set to double so integer division is correct real number
+            SetDefaultNumberType(DefaultNumberType.Double);
+           
+            //Create instances of the delegates
+            Function1d squareDel = ScientificCalculator.Math.Square;
+            Function1d sqrtDel = ScientificCalculator.Math.Sqrt;
+            Function1d absDel = ScientificCalculator.Math.Abs;
+            FunctionNd meanDel = ScientificCalculator.Math.Mean;
+            FunctionNd stddevDel = ScientificCalculator.Math.StdDev;
+            GetArray getArrayDel = GetArr;
+
+            //Bind the delegates to the tokens in the expression
+            SetFunction("Square", squareDel);
+            SetFunction("Sqrt", sqrtDel);
+            SetFunction("Abs", absDel);
+            SetFunction("Mean", meanDel);
+            SetFunction("StdDev", stddevDel);
+            SetFunction("GetArr", getArrayDel);
+            SetVariable("ans", answer);
+            SetVariable("arr", arr);
+
+        }
+
+
+        public void LoadArrayFromCSV(String fileName)
+        {
+            //Incomplete
+            using (var reader = new StreamReader(@fileName))
+            {
+                List<double> arrList = new List<double>();
+                while (!reader.EndOfStream)
+                {
+                    String line = reader.ReadLine();
+                    arrList.Add(Double.Parse(line));
+                }
+                arr = arrList.ToArray();
+                SetVariable("arr", arr);
+            }
+        }
+
+        public double[] GetArr(params double[] x)
+        {
+            return x;
+        }
+
+        public void SetArr(params double[] x)
+        {
+            arr = x;
+            SetVariable("arr", arr);
+        }
+
+        public double EvaluateString(String expression)
+        {
+            expression = expression.Replace("x", "*");
+            expression = expression.Replace("σ", "StdDev");
+            expression = expression.Replace("√", "Sqrt");
+            answer = Eval<double>(expression);
+            SetVariable("ans", answer);
+            return answer;
+        }
+
+        public void ConsoleUpdate()
+        {
+            String expression = "";
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter 0: set a list, 1: evaluate an expression \n");
+                    String option = Console.ReadLine();
+                    if (option.Equals("0"))
+                    {
+                        Console.WriteLine("Enter a list of numbers separated by a comma (eg. 1, 2, 3):\n");
+                        expression = Console.ReadLine();
+                        expression = "GetArr(" + expression + ")";
+                        arr = Eval<double[]>(expression);
+                        SetVariable("arr", arr);
+                    }
+                    else if (option.Equals("1"))
+                    {
+                        Console.WriteLine("Enter a math expression: \n");
+                        expression = Console.ReadLine();
+                        expression = expression.Replace("x", "*");
+                        EvaluateString(expression);
+                        Console.WriteLine("\nanswer = " + answer + "\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+    }
+}
---
+++ ScientificCalculator/Math.cs
@@ -0,0 +1,68 @@
+﻿
+
+namespace ScientificCalculator
+{
+	class Math
+	{
+		private const double TOLERANCE = 0.0000000001;
+
+		public static double Abs(double x)
+		{
+			return x >= 0.0 ? x : -x;
+		}
+
+		public static double Round(double x, uint place)
+        {
+			uint i;
+			for (i = 0; i<place;i++)
+				x *= 10;
+			x = (double)((decimal)(x + 0.5));
+			for (i = 0; i < place; i++)
+				x *= 0.1;
+			return x;
+		}
+
+		public static double Sqrt(double x)
+		{
+			double sqrt;
+			double x0 = x;
+
+			while (true)
+			{
+				sqrt = 0.5 * (x + (x0 / x));
+				if (Abs(sqrt - x) < TOLERANCE)
+					break;
+				x = sqrt;
+			}
+			return sqrt;
+		}
+
+		public static double Square(double x)
+		{
+			return x * x;
+		}
+
+		public static double Mean(params double[] x)
+		{
+			double mean = 0.0;
+			for (uint i = 0; i < x.Length; i++)
+				mean += x[i];
+			return mean / x.Length;
+		}
+
+		public static double StdDev(params double[] x)
+		{
+			int n = x.Length;
+			if (n > 0)
+			{
+				double sigma = 0.0;
+				double _mean = Mean(x);
+				for (uint i = 0; i < n; i++)
+					sigma += Square(x[i] - _mean);
+				return Sqrt(sigma / n);
+			}
+			else
+				return 0.0;
+		}
+	}
+}
